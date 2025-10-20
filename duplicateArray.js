0f83cdc4361f969dccc371cd634183ea03750d02
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2025-10-20 23:55:59 +0530

    duplicateArray O(n log n) time complexity added

--- duplicateArray.js
+++ duplicateArray.js
@@ -2,13 +2,24 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function (nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] == nums[j]) {
+//                 return true
+//             }
+//         }
+//     }
+//     return false
+// };
+
 var containsDuplicate = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] == nums[j]) {
+    nums = nums.sort((a,b) => a - b)
+    for (let i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
                 return true
             }
-        }
+        
     }
     return false
 };
